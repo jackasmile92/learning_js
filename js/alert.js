634b78040e515304d50b6b33914df85a941e9942
jackasmile92 <antonova.ekaterina@accenture.com>
2020-03-02 11:56:42 +0300

    testing commin from visual studio

---
+++ js/alert.js
@@ -0,0 +1,28 @@
+"use strict";
+/*let admin
+    , name;
+    name = "John";
+    admin = name;
+  //  alert(admin);
+*/
+  let name = "Иван";
+
+// Вставим переменную
+//alert( `Привет, ${name}!` ); // Привет, Иван!
+
+// Вставим выражение
+//alert( `результат: ${1 + 2}` ); // результат: 3
+
+
+/*let isGreater = 4 > 1;
+
+alert( isGreater );*/
+
+
+let str = "123";
+alert(typeof str); // string
+
+let num = Number(str); // становится числом 123
+
+alert(typeof num); // number
+alert("why?")
