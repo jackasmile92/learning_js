8496a4007310d247d2e7491eb23af1c9482f0d4b
jackasmile92 <antonova.ekaterina@accenture.com>
2020-03-03 17:47:31 +0300

    1

--- js/log.js
+++ js/log.js
@@ -1,7 +1,10 @@
 //alert('AAAAAAAA');
 //alert(varts);
-alert(input.length);
-
+//alert(input.length);
+ var btn;
 for (var prop in input) {
     console.log(input[prop].id);
+   btn  = document.createElement("BUTTON");   // Create a <button> element
+btn.innerHTML = input[prop].name;                   // Insert text
+document.body.appendChild(btn);  
   }
